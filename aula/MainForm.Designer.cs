7529c5b416dd31b25febbd37ecca0b1e877e8cc6
DouglasRamosP <douglasramospimenta@gmail.com>
2020-05-25 20:21:15 -0300

    first commit

---
+++ aula/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Wariston Pereira
+ * Date: 11/05/2020
+ * Time: 20:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace aula
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Buscar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 74);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(519, 185);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "id_estado",
+            "uf",
+            "nome"});
+            this.comboBox1.Location = new System.Drawing.Point(267, 42);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(267, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Em";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 267);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "aula";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+	}
+}
---
+++ aula/MainForm.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Wariston Pereira
+ * Date: 11/05/2020
+ * Time: 20:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+using MySql.Data;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace aula
+{
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+        }
+        void Button1Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder str = new MySqlConnectionStringBuilder();
+            str.Server = "remotemysql.com";
+            str.UserID = "269CpcWeCz";
+            str.Password = "cfW77mMFx4";
+            str.Database = "269CpcWeCz";
+
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString = str.ConnectionString;
+
+            con.Open();
+
+            MySqlCommand com = new MySqlCommand();
+            com.Connection = con;
+            if (comboBox1.Text == "uf")
+            {
+                com.CommandText = "SELECT * FROM estados WHERE uf LIKE'" + textBox1.Text + "%'";
+
+            }
+            else if (comboBox1.Text == "id_estado")
+            {
+                com.CommandText = "SELECT * FROM estados WHERE id_estado LIKE'" + textBox1.Text + "%'";
+
+            }
+            else if (comboBox1.Text == "nome")
+            {
+                com.CommandText = "SELECT * FROM estados WHERE nome LIKE'" + textBox1.Text + "%'";
+            }
+            else
+            {
+                com.CommandText = "SELECT * FROM estados ";
+            }
+
+            // Usada para comandos que não possuem retorno
+            // com.ExecuteNonQuery();
+
+            // Carrega o resultado da consulta para o reader
+            MySqlDataReader dr = com.ExecuteReader();
+
+            // Tabela virtual
+            DataTable dt = new DataTable();
+
+            // Carrega dados do reader para tabela virtual
+            dt.Load(dr);
+
+            // Atribuição dos dados ao grid
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }    
+    
+}
